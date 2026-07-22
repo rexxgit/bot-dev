245633db10a7d5e8ba422ff2081b83a5fefefcf5
Bisrat Tewabe Gedlie <rexxtewabe1@gmail.com>
2026-07-22 15:48:29 +0300

    Update trigger.js
    done

--- api/trigger.js
+++ api/trigger.js
@@ -1,5 +1,6 @@
 // api/trigger.js
 export default async function handler(req, res) {
+    // Only allow POST
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method not allowed' });
     }
